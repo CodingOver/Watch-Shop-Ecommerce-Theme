b4128cd8dd6895d2eaf2e597cb87fa51a89dacb8
Ayoub Fellat <paypal1business.over@gmail.com>
2021-02-16 22:40:59 +0100

    Add New Feature

--- js/main.js
+++ js/main.js
@@ -17,3 +17,64 @@ logoutBtn.addEventListener('click', function() {
     });
 });
 
+
+// Define Prodcut
+let productItems = document.querySelector('.product-itmes');
+let prodcuts = [
+    {
+        id: 1,
+        title: "Thermo Ball Etip Gloves",
+        price: "$ 45,743",
+        imageUrl: "https://preview.colorlib.com/theme/timezone/assets/img/gallery/new_product1.png"
+    },
+    {
+        id: 2,
+        title: "Thermo Ball Etip Gloves",
+        price: "$ 45,743",
+        imageUrl: "https://preview.colorlib.com/theme/timezone/assets/img/gallery/new_product2.png"
+    },
+    {
+        id: 3,
+        title: "Thermo Ball Etip Gloves",
+        price: "$ 45,743",
+        imageUrl: "https://preview.colorlib.com/theme/timezone/assets/img/gallery/new_product3.png"
+    },
+];
+
+function drawProductsUI () {
+    let productUI = prodcuts.map( item => {
+        return `
+            <div class="item">
+              <div class="image">
+                <img
+                  src="${item.imageUrl}"
+                  alt="${item.title}"
+                />
+                <div class="icons">
+                  <i class="fas fa-shopping-cart" onclick="addedToCart(${item.id})"></i>
+                  <i class="fas fa-heart"></i>
+                </div>
+              </div>
+              <p>${item.title}</p>
+              <span class="price">${item.price}</span>
+            </div>
+        `;
+    });
+    productItems.innerHTML = productUI.join('');
+}
+drawProductsUI();
+
+// Check If User Has An Account
+function CheckIfUserHasAccount() {
+    if(localStorage.getItem('username')) {
+        window.location = 'checkout.html';
+    } else {
+        window.location = 'login.html';
+    }
+}
+
+// Added To Cart 
+function addedToCart(id) {
+    let choosenItem = prodcuts.filter(item => item.id === id);
+    console.log(choosenItem);
+}
