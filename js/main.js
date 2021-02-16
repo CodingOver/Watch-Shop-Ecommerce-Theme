d3c5ff46c5e9ac68b4ce21d0215e52c3ab6126d2
Ayoub Fellat <paypal1business.over@gmail.com>
2021-02-16 13:54:33 +0100

    solve some issue in the main.js & create a slider of the index.html

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 let userInfo = document.querySelector('#user_info');
 let userDom = document.querySelector('#user');
 let links = document.querySelector('#links');
-let  user = localStorage.getItem('username');
-let userCapitalized = user.charAt(0).toUpperCase() + user.slice(1);
+let user = localStorage.getItem('username');
 let logoutBtn = document.querySelector('#logout');
 
 if(localStorage.getItem('username')) {
     links.remove();
     userInfo.style.display = 'flex';
-    userDom.innerHTML =  `Hi ${userCapitalized}`;
+    userDom.innerHTML =  `Hi ${user.charAt(0).toUpperCase() + user.slice(1)}`;
 }
 
 logoutBtn.addEventListener('click', function() {
